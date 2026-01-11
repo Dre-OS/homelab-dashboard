3f77d3d13c29eeddba807540ff4967d9c4de21b0
Dre-OS <65882951+Dre-OS@users.noreply.github.com>
2026-01-11 13:43:10 +0800

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';  
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="" element={}></Route>
+          <Route></Route> 
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
